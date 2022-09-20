1755189e0f5948505bc91bc090eed2d0e001c797
Christophe Fergeau <cfergeau@redhat.com>
2022-09-20 16:42:55 +0200

    serial: Add @available annotations
    This adds @available(macOS 11, *) annotations to the classes related to
    virtio-serial to indicate virtualization framework is only available in
    macOS 11+
    The corresponding go NewXXX methods return ErrUnsupportedOSVersion when
    running on macOS 10 systems.

--- virtualization.m
+++ virtualization.m
@@ -236,7 +236,9 @@ void setNetworkDevicesVZVirtualMachineConfiguration(void *config,
 void setSerialPortsVZVirtualMachineConfiguration(void *config,
     void *serialPorts)
 {
-    [(VZVirtualMachineConfiguration *)config setSerialPorts:[(NSMutableArray *)serialPorts copy]];
+    if (@available(macOS 11, *)) {
+        [(VZVirtualMachineConfiguration *)config setSerialPorts:[(NSMutableArray *)serialPorts copy]];
+    }
 }
 
 /*!
@@ -411,15 +413,19 @@ void *newVZGenericPlatformConfiguration()
 */
 void *newVZFileHandleSerialPortAttachment(int readFileDescriptor, int writeFileDescriptor)
 {
-    VZFileHandleSerialPortAttachment *ret;
-    @autoreleasepool {
-        NSFileHandle *fileHandleForReading = [[NSFileHandle alloc] initWithFileDescriptor:readFileDescriptor];
-        NSFileHandle *fileHandleForWriting = [[NSFileHandle alloc] initWithFileDescriptor:writeFileDescriptor];
-        ret = [[VZFileHandleSerialPortAttachment alloc]
-            initWithFileHandleForReading:fileHandleForReading
-                    fileHandleForWriting:fileHandleForWriting];
+    if (@available(macOS 11, *)) {
+        VZFileHandleSerialPortAttachment *ret;
+        @autoreleasepool {
+            NSFileHandle *fileHandleForReading = [[NSFileHandle alloc] initWithFileDescriptor:readFileDescriptor];
+            NSFileHandle *fileHandleForWriting = [[NSFileHandle alloc] initWithFileDescriptor:writeFileDescriptor];
+            ret = [[VZFileHandleSerialPortAttachment alloc]
+                initWithFileHandleForReading:fileHandleForReading
+                        fileHandleForWriting:fileHandleForWriting];
+        }
+        return ret;
+    } else {
+            return nil;
     }
-    return ret;
 }
 
 /*!
@@ -432,16 +438,21 @@ void *newVZFileHandleSerialPortAttachment(int readFileDescriptor, int writeFileD
  */
 void *newVZFileSerialPortAttachment(const char *filePath, bool shouldAppend, void **error)
 {
-    VZFileSerialPortAttachment *ret;
-    @autoreleasepool {
-        NSString *filePathNSString = [NSString stringWithUTF8String:filePath];
-        NSURL *fileURL = [NSURL fileURLWithPath:filePathNSString];
-        ret = [[VZFileSerialPortAttachment alloc]
-            initWithURL:fileURL
-                 append:(BOOL)shouldAppend
-                  error:(NSError *_Nullable *_Nullable)error];
+    if (@available(macOS 11, *)) {
+        VZFileSerialPortAttachment *ret;
+        @autoreleasepool {
+            NSString *filePathNSString = [NSString stringWithUTF8String:filePath];
+            NSURL *fileURL = [NSURL fileURLWithPath:filePathNSString];
+            ret = [[VZFileSerialPortAttachment alloc]
+                initWithURL:fileURL
+                     append:(BOOL)shouldAppend
+                      error:(NSError *_Nullable *_Nullable)error];
+        }
+        return ret;
+    } else {
+        return nil;
     }
-    return ret;
+
 }
 
 /*!
@@ -454,9 +465,13 @@ void *newVZFileSerialPortAttachment(const char *filePath, bool shouldAppend, voi
  */
 void *newVZVirtioConsoleDeviceSerialPortConfiguration(void *attachment)
 {
-    VZVirtioConsoleDeviceSerialPortConfiguration *config = [[VZVirtioConsoleDeviceSerialPortConfiguration alloc] init];
-    [config setAttachment:(VZSerialPortAttachment *)attachment];
-    return config;
+    if (@available(macOS 11, *)) {
+        VZVirtioConsoleDeviceSerialPortConfiguration *config = [[VZVirtioConsoleDeviceSerialPortConfiguration alloc] init];
+        [config setAttachment:(VZSerialPortAttachment *)attachment];
+        return config;
+    } else {
+        return nil;
+    }
 }
 
 /*!
