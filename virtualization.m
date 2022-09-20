cf3320da517e56adc7cc8b5f3e6f8e5fd01c965f
Christophe Fergeau <cfergeau@redhat.com>
2022-09-20 16:43:16 +0200

    storage: Add @available annotations
    This adds @available(macOS 11, *) annotations to the classes related to
    virtio-blk to indicate virtualization framework is only available in
    macOS 11+
    The corresponding go NewXXX methods return ErrUnsupportedOSVersion when
    running on macOS 10 systems.

--- virtualization.m
+++ virtualization.m
@@ -258,7 +258,9 @@ void setSocketDevicesVZVirtualMachineConfiguration(void *config,
 void setStorageDevicesVZVirtualMachineConfiguration(void *config,
     void *storageDevices)
 {
-    [(VZVirtualMachineConfiguration *)config setStorageDevices:[(NSMutableArray *)storageDevices copy]];
+    if (@available(macOS 11, *)) {
+        [(VZVirtualMachineConfiguration *)config setStorageDevices:[(NSMutableArray *)storageDevices copy]];
+    }
 }
 /*!
  @abstract List of directory sharing devices. Empty by default.
@@ -577,7 +579,11 @@ void *newVZVirtioEntropyDeviceConfiguration()
  */
 void *newVZVirtioBlockDeviceConfiguration(void *attachment)
 {
-    return [[VZVirtioBlockDeviceConfiguration alloc] initWithAttachment:(VZStorageDeviceAttachment *)attachment];
+    if (@available(macOS 11, *)) {
+        return [[VZVirtioBlockDeviceConfiguration alloc] initWithAttachment:(VZStorageDeviceAttachment *)attachment];
+    } else {
+        return nil;
+    }
 }
 
 /*!
@@ -589,12 +595,16 @@ void *newVZVirtioBlockDeviceConfiguration(void *attachment)
  */
 void *newVZDiskImageStorageDeviceAttachment(const char *diskPath, bool readOnly, void **error)
 {
-    NSString *diskPathNSString = [NSString stringWithUTF8String:diskPath];
-    NSURL *diskURL = [NSURL fileURLWithPath:diskPathNSString];
-    return [[VZDiskImageStorageDeviceAttachment alloc]
-        initWithURL:diskURL
-           readOnly:(BOOL)readOnly
-              error:(NSError *_Nullable *_Nullable)error];
+    if (@available(macOS 11, *)) {
+        NSString *diskPathNSString = [NSString stringWithUTF8String:diskPath];
+        NSURL *diskURL = [NSURL fileURLWithPath:diskPathNSString];
+        return [[VZDiskImageStorageDeviceAttachment alloc]
+            initWithURL:diskURL
+               readOnly:(BOOL)readOnly
+                  error:(NSError *_Nullable *_Nullable)error];
+    } else {
+        return nil;
+    }
 }
 
 /*!
