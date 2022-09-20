b1a406fba362684e8386eaf377362703961c9d46
Christophe Fergeau <cfergeau@redhat.com>
2022-09-20 16:42:55 +0200

    serial: Add @available annotations
    This adds @available(macOS 11, *) annotations to the classes related to
    virtio-serial to indicate virtualization framework is only available in
    macOS 11+
    The corresponding go NewXXX methods return ErrUnsupportedOSVersion when
    running on macOS 10 systems.

--- example/linux/main.go
+++ example/linux/main.go
@@ -78,8 +78,14 @@ func main() {
 	setRawMode(os.Stdin)
 
 	// console
-	serialPortAttachment := vz.NewFileHandleSerialPortAttachment(os.Stdin, os.Stdout)
-	consoleConfig := vz.NewVirtioConsoleDeviceSerialPortConfiguration(serialPortAttachment)
+	serialPortAttachment, err := vz.NewFileHandleSerialPortAttachment(os.Stdin, os.Stdout)
+	if err != nil {
+		log.Fatalf("Serial port attachment creation failed: %s", err)
+	}
+	consoleConfig, err := vz.NewVirtioConsoleDeviceSerialPortConfiguration(serialPortAttachment)
+	if err != nil {
+		log.Fatalf("Failed to create serial configuration: %s", err)
+	}
 	config.SetSerialPortsVirtualMachineConfiguration([]*vz.VirtioConsoleDeviceSerialPortConfiguration{
 		consoleConfig,
 	})
