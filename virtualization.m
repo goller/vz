38626f2bfe55d08b18599a1dec55e13a3cb31485
Christophe Fergeau <cfergeau@redhat.com>
2022-09-22 14:53:41 +0200

    bootloader: Add @available annotations
    This adds @available(macOS 11, *) annotations to the classes related to
    linux bootloaders to indicate virtualization framework is only available
    in macOS 11+
    The corresponding go NewXXX methods return ErrUnsupportedOSVersion when
    running on macOS 10 systems.

--- virtualization.m
+++ virtualization.m
@@ -54,13 +54,17 @@ char *copyCString(NSString *nss)
 */
 void *newVZLinuxBootLoader(const char *kernelPath)
 {
-    VZLinuxBootLoader *ret;
-    @autoreleasepool {
-        NSString *kernelPathNSString = [NSString stringWithUTF8String:kernelPath];
-        NSURL *kernelURL = [NSURL fileURLWithPath:kernelPathNSString];
-        ret = [[VZLinuxBootLoader alloc] initWithKernelURL:kernelURL];
+    if (@available(macOS 11, *)) {
+        VZLinuxBootLoader *ret;
+        @autoreleasepool {
+            NSString *kernelPathNSString = [NSString stringWithUTF8String:kernelPath];
+            NSURL *kernelURL = [NSURL fileURLWithPath:kernelPathNSString];
+            ret = [[VZLinuxBootLoader alloc] initWithKernelURL:kernelURL];
+        }
+        return ret;
+    } else {
+            return nil;
     }
-    return ret;
 }
 
 /*!
@@ -71,10 +75,12 @@ void *newVZLinuxBootLoader(const char *kernelPath)
  */
 void setCommandLineVZLinuxBootLoader(void *bootLoaderPtr, const char *commandLine)
 {
-    VZLinuxBootLoader *bootLoader = (VZLinuxBootLoader *)bootLoaderPtr;
-    @autoreleasepool {
-        NSString *commandLineNSString = [NSString stringWithUTF8String:commandLine];
-        [bootLoader setCommandLine:commandLineNSString];
+    if (@available(macOS 11, *)) {
+        VZLinuxBootLoader *bootLoader = (VZLinuxBootLoader *)bootLoaderPtr;
+        @autoreleasepool {
+            NSString *commandLineNSString = [NSString stringWithUTF8String:commandLine];
+            [bootLoader setCommandLine:commandLineNSString];
+        }
     }
 }
 
@@ -86,11 +92,13 @@ void setCommandLineVZLinuxBootLoader(void *bootLoaderPtr, const char *commandLin
  */
 void setInitialRamdiskURLVZLinuxBootLoader(void *bootLoaderPtr, const char *ramdiskPath)
 {
-    VZLinuxBootLoader *bootLoader = (VZLinuxBootLoader *)bootLoaderPtr;
-    @autoreleasepool {
-        NSString *ramdiskPathNSString = [NSString stringWithUTF8String:ramdiskPath];
-        NSURL *ramdiskURL = [NSURL fileURLWithPath:ramdiskPathNSString];
-        [bootLoader setInitialRamdiskURL:ramdiskURL];
+    if (@available(macOS 11, *)) {
+        VZLinuxBootLoader *bootLoader = (VZLinuxBootLoader *)bootLoaderPtr;
+        @autoreleasepool {
+            NSString *ramdiskPathNSString = [NSString stringWithUTF8String:ramdiskPath];
+            NSURL *ramdiskURL = [NSURL fileURLWithPath:ramdiskPathNSString];
+            [bootLoader setInitialRamdiskURL:ramdiskURL];
+        }
     }
 }
 
